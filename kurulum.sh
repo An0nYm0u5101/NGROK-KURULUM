8b52d2b55e18f5b747f44f5fc519ea5b8d92c27f
u0_a221 <u0_a221@localhost>
2020-07-03 00:52:00 +0300

    first commit

---
+++ kurulum.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+mv ngrok /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod 777 ngrok
+cd -
+cd ..
+rm -rf ngrok
+clear
+echo
+echo
+echo
+printf "\e[32m
+
+K  K U   U RRRR  U   U L    U   U M   M 
+K K  U   U R   R U   U L    U   U MM MM 
+KK   U   U RRRR  U   U L    U   U M M M 
+K K  U   U R R   U   U L    U   U M   M 
+K  K  UUU  R  RR  UUU  LLLL  UUU  M   M 
+
+
+TTTTTT  AA  M   M  AA  M   M L     AA  N   N DDD  III
+  TT   A  A MM MM A  A MM MM L    A  A NN  N D  D  I
+  TT   AAAA M M M AAAA M M M L    AAAA N N N D  D  I
+  TT   A  A M   M A  A M   M L    A  A N  NN D  D  I
+  TT   A  A M   M A  A M   M LLLL A  A N   N DDD  III
+
+\e[0m"
